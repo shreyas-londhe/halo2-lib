4fcc1336f4a279f343b94f3b656732d082497820
shreyas-londhe <shreyas_londhe@outlook.com>
2024-05-24 06:56:16 +0530

    test: poseidon hash2curve

--- halo2-ecc/src/secp256k1/hash_to_curve/mod.rs
+++ halo2-ecc/src/secp256k1/hash_to_curve/mod.rs
@@ -40,10 +40,25 @@ pub fn hash_to_curve<F: BigPrimeField>(
 mod test {
     use halo2_base::{
         gates::RangeInstructions,
-        halo2_proofs::halo2curves::bn256::Fr,
+        halo2_proofs::{
+            arithmetic::CurveAffine,
+            halo2curves::{
+                bn256::Fr,
+                secp256k1::{Fp, Secp256k1Affine},
+            },
+        },
         poseidon::hasher::{spec::OptimizedPoseidonSpec, PoseidonHasher},
-        utils::testing::base_test,
+        utils::{testing::base_test, ScalarField},
     };
+
+    use k256::{
+        elliptic_curve::{
+            hash2curve::{ExpandMsgXmd, GroupDigest},
+            sec1::ToEncodedPoint,
+        },
+        Secp256k1,
+    };
+    use pse_poseidon::Poseidon;
 
     use crate::{ecc::EccChip, secp256k1::FpChip};
 
@@ -51,15 +66,42 @@ mod test {
 
     #[derive(Default, Clone)]
     struct TestData {
-        message: [u8; 4],
+        message: [u8; 11],
+        pt: ([u8; 32], [u8; 32]),
     }
 
     #[test]
     fn test_hash_to_curve() {
+        let message = "hello world";
+        let pt = Secp256k1::hash_from_bytes::<ExpandMsgXmd<Poseidon<Fr, 3, 2>>>(
+            &[message.as_bytes()],
+            &[b"QUUX-V01-CS02-with-secp256k1_XMD:SHA-256_SSWU_RO_"],
+        )
+        .unwrap()
+        .to_affine();
+        let pt = pt.to_encoded_point(false).to_bytes().into_vec();
+        assert_eq!(pt.len(), 65);
+
+        let mut x = pt[1..33].to_vec();
+        x.reverse();
+        let mut y = pt[33..].to_vec();
+        y.reverse();
+
+        let pt = Secp256k1Affine::from_xy(
+            Fp::from_bytes_le(x.as_slice()),
+            Fp::from_bytes_le(y.as_slice()),
+        )
+        .unwrap();
+
+        let test_data = TestData {
+            message: message.as_bytes().try_into().unwrap(),
+            pt: (pt.x.to_bytes_le().try_into().unwrap(), pt.y.to_bytes_le().try_into().unwrap()),
+        };
+
         base_test().k(15).lookup_bits(14).expect_satisfied(true).bench_builder(
             TestData::default(),
-            TestData { message: [0x01, 0x02, 0x03, 0x04] },
-            |pool, range, data: TestData| {
+            test_data.clone(),
+            |pool, range, _: TestData| {
                 let ctx = pool.main();
 
                 let fp_chip = FpChip::<Fr>::new(range, 88, 3);
@@ -69,13 +111,16 @@ mod test {
                     PoseidonHasher::<Fr, 3, 2>::new(OptimizedPoseidonSpec::new::<8, 57, 0>());
                 poseidon_hasher.initialize_consts(ctx, range.gate());
 
-                let msg_bytes = data
+                let msg_bytes = test_data
                     .message
                     .iter()
                     .map(|&x| ctx.load_witness(Fr::from(x as u64)))
                     .collect::<Vec<_>>();
 
                 let point = hash_to_curve(ctx, &ecc_chip, &poseidon_hasher, msg_bytes.as_slice());
+
+                assert_eq!(point.x().value().to_bytes_le(), test_data.pt.0);
+                assert_eq!(point.y().value().to_bytes_le(), test_data.pt.1);
             },
         );
     }
